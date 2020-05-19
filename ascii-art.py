f6918f475c8442e23e77479fad7a88a0341b4941
Mihir <mihir.xtha@gmail.com>
2020-05-18 21:48:32 -0400

    added comments

--- ascii-art.py
+++ ascii-art.py
@@ -3,21 +3,23 @@ from pyperclip import copy
 import os
 import sys
 
-MAX_HEIGHT = 100
-MAX_WIDTH = 300
-IMAGE_NAME = 'dog.png'
+MAX_HEIGHT = 100  # maximum height image will be resized to
+MAX_WIDTH = 300  # maximum width image will be resized to
+IMAGE_NAME = 'dog.png'  # image name we will convert to ascii
 IMAGE_PATH = os.path.join('Images', IMAGE_NAME)  # image path that you want to open
 FOLDER_NAME = 'ASCII Files'  # folder we will save images to
 CHOICE = "LUMINOSITY"  # AVERAGE, LIGHTNESS, AND LUMINOSITY are possible choices
 WRITE_TO_FILE = True  # setting to write to file
 COPY_TO_CLIPBOARD = False  # setting to copy to clipboard
-PRINT_TO_SCREEN = False
+PRINT_TO_SCREEN = False  # setting to print to screen
 INVERTED = False  # setting to invert ascii-txt
 
+# characters that range from least to most visible
 characters = r"`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 inverseCharacters = characters[::-1]  # same array as above but in reverse
 CONSTANT = 3.85  # constant we will use to determine which character to use
 
+# if run from terminal, accept the arguments
 if len(sys.argv) > 1:
     IMAGE_PATH = sys.argv[1]
     additionalFlags = sys.argv[2:]
@@ -31,35 +33,35 @@ if len(sys.argv) > 1:
 
 
 def main():
-    baseFile = os.path.basename(IMAGE_PATH)
+    baseFile = os.path.basename(IMAGE_PATH)  # get base name of the image file
     name, extension = baseFile.split('.')  # get name and extension of image
     if INVERTED:
-        name += '-inverted'  # add inverted
-    name += f'-{CHOICE.lower()}'  # add choice name to name
+        name += '-inverted'  # add inverted to file name
+    name += f'-{CHOICE.lower()}'  # add choice name to file name
     FILE_NAME = f'{name}.txt'  # file name we will save ascii image as
 
     image = Image.open(IMAGE_PATH)  # open the image
     if image.mode == 'P':  # if transparent, convert to RGBA
         image = image.convert('RGBA')
     else:
-        image = image.convert('RGB')   # force convert to RGB
+        image = image.convert('RGB')   # else force convert to RGB
     image.thumbnail((MAX_WIDTH, MAX_HEIGHT))  # Resizing image
     width, height = image.size  # get width and height of image
 
     pixels = list(image.getdata())  # get list of RGB values from image
     matrix = [[pixels[width * x + y] for y in range(width)] for x in range(height)]  # get 2D array of RGB values
 
-    if CHOICE == "AVERAGE":
+    if CHOICE == "AVERAGE":  # use average formula to get brightness array
         brightnessMatrix = [[get_average(matrix[x][y]) for y in range(width)] for x in range(height)]
-    elif CHOICE == "LIGHTNESS":
+    elif CHOICE == "LIGHTNESS":  # use lightness formula to get brightness array
         brightnessMatrix = [[get_lightness(matrix[x][y]) for y in range(width)] for x in range(height)]
-    else:
+    else:  # use luminosity formula to get brightness array
         brightnessMatrix = [[get_luminosity(matrix[x][y]) for y in range(width)] for x in range(height)]
 
-    if INVERTED:
-        characterMatrix = [[get_characterIndex(brightnessMatrix[x][y], True) for y in range(width)] for x in range(height)]
-    else:
-        characterMatrix = [[get_characterIndex(brightnessMatrix[x][y], False) for y in range(width)] for x in range(height)]
+    if INVERTED:  # if inverted, get character from the reversed list
+        characterMatrix = [[get_character(brightnessMatrix[x][y], True) for y in range(width)] for x in range(height)]
+    else:  # if not inverted, get character from the original list
+        characterMatrix = [[get_character(brightnessMatrix[x][y], False) for y in range(width)] for x in range(height)]
 
     totalString = ''
 
@@ -70,7 +72,7 @@ def main():
         copy(totalString)  # copy ascii-string to clipboard
         print("ASCII image copied to clipboard.")
 
-    if WRITE_TO_FILE:
+    if WRITE_TO_FILE:  # write to a file
         if not os.path.exists(FOLDER_NAME):
             os.mkdir(FOLDER_NAME)
 
@@ -86,7 +88,7 @@ def main():
             f.write(totalString)
             print(f"ASCII image saved to {os.path.abspath(FILE_NAME)}.")
 
-    if PRINT_TO_SCREEN:
+    if PRINT_TO_SCREEN:  # print to screen
         print(totalString)
 
 
@@ -105,7 +107,8 @@ def get_luminosity(rgbTuple):
     return rgbTuple[0] * .21 + rgbTuple[1] * .72 + rgbTuple[2] * .07
 
 
-def get_characterIndex(brightness, inverse):
+# helper function to get character depending on brightness and inverse values
+def get_character(brightness, inverse):
     index = round(brightness / CONSTANT)
     if inverse:
         return inverseCharacters[index]
