d7995c6195a2ec8187d198268cdd3de973ae0004
mshrestha <mihir.xtha@gmail.com>
2020-06-03 00:49:44 -0400

    small bug fix

--- ascii-art/run.py
+++ ascii-art/run.py
@@ -5,19 +5,24 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='Convert an image to ASCII')
     parser.add_argument('imagePath', type=str, help='path to image file that will be converted')
-    parser.add_argument('-i', '--inverted', help='invert the generated ASCII',
+    parser.add_argument('--inverted', help='invert the generated ASCII',
                         action="store_true")
-    parser.add_argument('-c', '--copy', help='copy the generated ASCII to clipboard',
+    parser.add_argument('--copy', help='copy the generated ASCII to clipboard',
                         action="store_true")
     parser.add_argument('--choice', help='pick what type of algorithm to use',
-                        nargs=1, default="average", choices=('luminosity', 'brightness', 'average'))
-    parser.add_argument('-p', '--print', help='print the generated ASCII with specified color if provided',
+                        nargs=1, default=["average"], choices=('luminosity', 'lightness', 'average'))
+    parser.add_argument('--height', help='specify height for generated ascii text',
+                        nargs=1, default=[50], type=int)
+    parser.add_argument('--width', help='specify width for generated ascii text',
+                        nargs=1, default=[50], type=int)
+    parser.add_argument('--print', help='print the generated ASCII with specified color if provided',
                         nargs='?', const=1, choices=('red', 'blue', 'yellow', 'green', 'cyan', 'magenta'))
-    parser.add_argument('-w', '--write', help='write the generated ASCII to specified file if provided or default file',
+    parser.add_argument('--write', help='write the generated ASCII to specified filename if provided',
                         nargs='?', const=1)
     args = parser.parse_args()
 
-    monkeyText = ASCIIArt(args.imagePath, inverted=args.inverted, choice=args.choice)
+    dimension = (args.width[0], args.height[0])
+    monkeyText = ASCIIArt(args.imagePath, inverted=args.inverted, choice=args.choice[0], dimension=dimension)
     if args.copy:
         monkeyText.copy_to_clipboard()
     if args.print:
